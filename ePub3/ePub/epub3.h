60db826db15087ba3df9f0b4f7d7311359d2ac63
Jim Dovey <jdovey@kobobooks.com>
2012-12-12 17:01:48 -0500

    Added namespace support to _getProp and added macros for common namespaces.

--- ePub3/ePub/epub3.h
+++ ePub3/ePub/epub3.h
@@ -12,15 +12,36 @@
 #define EPUB3_BEGIN_NAMESPACE namespace ePub3 {
 #define EPUB3_END_NAMESPACE };
 
+#define ePub3NamespaceURI "http://www.idpf.org/2007/ops"
+#define MathMLNamespaceURI "http://www.w3.org/1998/Math/MathML"
+#define PLSNamespaceURI "http://www.w3.org/2005/01/pronunciation-lexicon"
+#define SSMLNamespaceURI "http://www.w3.org/2001/10/synthesis"
+#define SVGNamespaceURI "http://www.w3.org/2000/svg"
+#define OCSNamespaceURI "urn:oasis:names:tc:opendocument:xmlns:container"
+#define XMLENCNamespaceURI "http://www.w3.org/2001/04/xmlenc#"
+#define XMLDSigNamespaceURI "http://www.w3.org/2000/09/xmldsig#"
+
 // this chunk will go away once we have the C++ XML interface complete
 #include <string>
 #include <libxml/tree.h>
 
 EPUB3_BEGIN_NAMESPACE
 
-static inline std::string _getProp(xmlNodePtr node, const char *name)
+static inline std::string _getProp(xmlNodePtr node, const char *name, const char *nsURI = nullptr)
 {
-    const xmlChar * ch = xmlGetProp(node, reinterpret_cast<const xmlChar*>(name));
+    if ( node == nullptr )
+        return "";
+    
+    const xmlChar * ch = nullptr;
+    if ( nsURI == nullptr )
+    {
+        ch = xmlGetProp(node, reinterpret_cast<const xmlChar*>(name));
+    }
+    else
+    {
+        ch = xmlGetNsProp(node, reinterpret_cast<const xmlChar*>(name), reinterpret_cast<const xmlChar*>(nsURI));
+    }
+    
     if ( ch == nullptr )
         return "";
     return reinterpret_cast<const char*>(ch);
