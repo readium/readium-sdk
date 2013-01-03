5e0eabcd6a02d088970baf758f8f6d20eaaf5fbb
Jim Dovey <jdovey@kobobooks.com>
2013-01-03 16:02:00 -0500

    Missed some deletions in the last commit.

--- ePub3/ePub/filter.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  filter.cpp
-//  ePub3
-//
-//  Created by Jim Dovey on 2012-12-21.
-//  Copyright (c) 2012 Kobo Inc. All rights reserved.
-//
-
-#include "filter.h"
--- ePub3/testing/main.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  main.cpp
-//  ePub3
-//
-//  Created by Jim Dovey on 2013-01-02.
-//  Copyright (c) 2013 Kobo Inc. All rights reserved.
-//
-
-#include <stdio.h>
