c2b8a4925f6d9474dee80ab3b594df1c90a57019
Daniel Weck <daniel.weck@gmail.com>
2013-09-15 22:31:40 +0100

    XML parsing mini progress (untested, just coding in the dark right now...)

--- ePub3/ePub/media-overlays_smil_model.cpp
+++ ePub3/ePub/media-overlays_smil_model.cpp
@@ -20,6 +20,7 @@
 //
 
 #include "media-overlays_smil_model.h"
+#include "xpath_wrangler.h"
 #include "package.h"
 
 EPUB3_BEGIN_NAMESPACE
@@ -31,48 +32,78 @@ static std::map<string, bool> AllowedRootNodeNames = {
     { "smil", true }
 };
 #else
-typedef std::pair<string, bool> __name_pair_t;
-static __name_pair_t __name_pairs[1] = {
-    __name_pair_t("smil", true)
-};
-static std::map<string, bool> AllowedRootNodeNames(&__name_pairs[0], &__name_pairs[1]);
+        typedef std::pair<string, bool> __name_pair_t;
+        static __name_pair_t __name_pairs[1] = {
+                __name_pair_t("smil", true)
+        };
+        static std::map<string, bool> AllowedRootNodeNames(&__name_pairs[0], &__name_pairs[1]);
 #endif
 
-MediaOverlaysSmilModel::MediaOverlaysSmilModel(shared_ptr<Package>& owner)
-{
-}
+        MediaOverlaysSmilModel::MediaOverlaysSmilModel(shared_ptr<Package>& owner)
+        : OwnedBy(owner)
+        {
+        }
 
-bool MediaOverlaysSmilModel::ParseXML(xmlNodePtr node)
-{
-    if ( node == nullptr )
-        return false;
-    
-    string name(node->name);
-    if ( AllowedRootNodeNames.find(name) == AllowedRootNodeNames.end() )
-        return false;
-    
+        bool MediaOverlaysSmilModel::ParseXML(xmlNodePtr node)
+        {
+            if (node == nullptr)
+                return false;
+
+            string name(node->name);
+            if (AllowedRootNodeNames.find(name) == AllowedRootNodeNames.end())
+                return false;
+
+            string version = _getProp(node, "version", SMILNamespaceURI);
+            if (!version.empty())
+            {
+                printf("SMIL VERSION: %s\n", version); //"3.0"
+            }
+            else
+            {
+                printf("NO SMIL VERSION?\n");
+            }
+
 #if EPUB_COMPILER_SUPPORTS(CXX_INITIALIZER_LISTS)
-    XPathWrangler xpath(node->doc, {{"epub", ePub3NamespaceURI}, {"smil", smilNamespaceURI}});
+    XPathWrangler xpath(node->doc, {{"epub", ePub3NamespaceURI}, {"smil", SMILNamespaceURI}});
 #else
-    XPathWrangler::NamespaceList __ns;
-    __ns["epub"] = ePub3NamespaceURI;
-    __ns["smil"] = smilNamespaceURI;
-    XPathWrangler xpath(node->doc, __ns);
+            XPathWrangler::NamespaceList __ns;
+            __ns["epub"] = ePub3NamespaceURI;
+            __ns["smil"] = SMILNamespaceURI;
+            XPathWrangler xpath(node->doc, __ns);
 #endif
-    xpath.NameDefaultNamespace("smil");
+            xpath.NameDefaultNamespace("smil");
 
-    xmlNodeSetPtr nodes = xpath.Nodes("./smil:body", node);
-    if ( nodes == nullptr )
-        return false;
-    if ( nodes->nodeNr != 1 )
-    {
-        xmlXPathFreeNodeSet(nodes);
-        return false;
-    }
+            xmlNodeSetPtr nodes = xpath.Nodes("./smil:body", node);
+            if (nodes == nullptr)
+                return false;
+            if (nodes->nodeNr != 1)
+            {
+                xmlXPathFreeNodeSet(nodes);
+                return false;
+            }
 
-    xmlXPathFreeNodeSet(nodes);
-    
-    return true;
-}
+            xmlNodePtr linkedChildrenList = nodes->nodeTab[0]->children;
+            if (linkedChildrenList == nullptr)
+            {
+                return false;
+            }
 
-EPUB3_END_NAMESPACE
+            for ( ; linkedChildrenList != nullptr; linkedChildrenList = linkedChildrenList->next )
+            {
+                if ( linkedChildrenList->type != XML_ELEMENT_NODE )
+                    continue;
+
+                std::string elementName(reinterpret_cast<const char*>(linkedChildrenList->name));
+
+                if ( elementName == "p" )
+                {
+                    const string& str = reinterpret_cast<const char*>(xmlNodeGetContent(linkedChildrenList));
+                }
+            }
+
+            xmlXPathFreeNodeSet(nodes);
+
+            return true;
+        }
+
+        EPUB3_END_NAMESPACE
