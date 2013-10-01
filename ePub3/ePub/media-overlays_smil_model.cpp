ef8ff93c89a54ed30ba7aa2ea0ff31fe87e63c7c
Daniel Weck <daniel.weck@gmail.com>
2013-10-01 08:57:13 +0100

    Skippables and Escapables now defined in the C++ core, exposed via public API for consumption in app layer (who in turns passes the string array to the Javascript layer via JSON). tested with LauncherOSX

--- ePub3/ePub/media-overlays_smil_model.cpp
+++ ePub3/ePub/media-overlays_smil_model.cpp
@@ -777,4 +777,72 @@ XPathWrangler xpath(doc, {{"epub", ePub3NamespaceURI}, {"smil", SMILNamespaceURI
             return Owner()->MediaOverlays_PlaybackActiveClass();
         }
 
+
+        const std::vector<string> MediaOverlaysSmilModel::_Skippables = std::vector<string>({
+                "sidebar",
+                "practice",
+                "marginalia",
+                "annotation",
+                "help",
+                "note",
+                "footnote",
+                "rearnote",
+                "table",
+                "table-row",
+                "table-cell",
+                "list",
+                "list-item",
+                "pagebreak"
+        });
+
+        const std::vector<string> MediaOverlaysSmilModel::_Escapables = std::vector<string>({
+                "sidebar",
+                "bibliography",
+                "toc",
+                "loi",
+                "appendix",
+                "landmarks",
+                "lot",
+                "index",
+                "colophon",
+                "epigraph",
+                "conclusion",
+                "afterword",
+                "warning",
+                "epilogue",
+                "foreword",
+                "introduction",
+                "prologue",
+                "preface",
+                "preamble",
+                "notice",
+                "errata",
+                "copyright-page",
+                "acknowledgments",
+                "other-credits",
+                "titlepage",
+                "imprimatur",
+                "contributors",
+                "halftitlepage",
+                "dedication",
+                "help",
+                "annotation",
+                "marginalia",
+                "practice",
+                "note",
+                "footnote",
+                "rearnote",
+                "footnotes",
+                "rearnotes",
+                "bridgehead",
+                "page-list",
+                "table",
+                "table-row",
+                "table-cell",
+                "list",
+                "list-item",
+                "glossary"
+        });
+
+
         EPUB3_END_NAMESPACE
