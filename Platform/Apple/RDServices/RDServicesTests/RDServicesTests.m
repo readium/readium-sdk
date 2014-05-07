f544ea0ccc040ab4339e3c34df19e6607926506c
John Nowak <johnnowak@nypl.org>
2014-05-07 11:51:33 -0400

    Added Xcode project with library product for RDServices

---
+++ Platform/Apple/RDServices/RDServicesTests/RDServicesTests.m
@@ -0,0 +1,34 @@
+//
+//  RDServicesTests.m
+//  RDServicesTests
+//
+//  Created by John Nowak on 5/7/14.
+//  Copyright (c) 2014 Readium Foundation. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RDServicesTests : XCTestCase
+
+@end
+
+@implementation RDServicesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
