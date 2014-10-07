ade9eecf31c79d5ef684594017ad436150b612e7
Daniel Weck <daniel.weck@gmail.com>
2014-10-07 11:32:29 +0100

    initial commit for feature branch htmlInject

--- Platform/Apple/RDServices/Main/RDPackageResourceConnection.m
+++ Platform/Apple/RDServices/Main/RDPackageResourceConnection.m
@@ -34,12 +34,82 @@
 #import "RDPackageResourceResponse.h"
 #import "RDPackageResourceServer.h"
 
-
 static RDPackage *m_package = nil;
 
+// The incremental numeral below is needed to prevent
+// iOS UIWebView / NSURLCache from auto-caching requests to "readium_epubReadingSystem_inject.js"
+// See implementation of:
+// -(NSCachedURLResponse *)cachedResponseForRequest:(NSURLRequest *)request
+// ... in NSURLCacheInterceptor
+// Note that OSX implementation does not require this hack,
+// as the following request interceptor method is provided natively by WebView (no caching):
+/*
+- (NSURLRequest*) webView:(WebView*)sender
+        resource:(id)identifier
+        willSendRequest:(NSURLRequest*)request
+        redirectResponse:(NSURLResponse*)redirectResponse
+        fromDataSource:(WebDataSource*)dataSource
+{
+...
+}
+*/
+static long m_injectCounter = 0;
 
 @implementation RDPackageResourceConnection
 
+//
+// Converts the given HTML data to a string.  The character set and encoding are assumed to be
+// UTF-8, UTF-16BE, or UTF-16LE.
+//
+- (NSString *)htmlFromData:(NSData *)data {
+    if (data == nil || data.length == 0) {
+        return nil;
+    }
+
+    NSString *html = nil;
+    UInt8 *bytes = (UInt8 *)data.bytes;
+
+    if (data.length >= 3) {
+        if (bytes[0] == 0xFE && bytes[1] == 0xFF) {
+            html = [[NSString alloc] initWithData:data
+                                         encoding:NSUTF16BigEndianStringEncoding];
+        }
+        else if (bytes[0] == 0xFF && bytes[1] == 0xFE) {
+            html = [[NSString alloc] initWithData:data
+                                         encoding:NSUTF16LittleEndianStringEncoding];
+        }
+        else if (bytes[0] == 0xEF && bytes[1] == 0xBB && bytes[2] == 0xBF) {
+            html = [[NSString alloc] initWithData:data
+                                         encoding:NSUTF8StringEncoding];
+        }
+        else if (bytes[0] == 0x00) {
+            // There's a very high liklihood of this being UTF-16BE, just without the BOM.
+            html = [[NSString alloc] initWithData:data
+                                         encoding:NSUTF16BigEndianStringEncoding];
+        }
+        else if (bytes[1] == 0x00) {
+            // There's a very high liklihood of this being UTF-16LE, just without the BOM.
+            html = [[NSString alloc] initWithData:data
+                                         encoding:NSUTF16LittleEndianStringEncoding];
+        }
+        else {
+            html = [[NSString alloc] initWithData:data
+                                         encoding:NSUTF8StringEncoding];
+
+            if (html == nil) {
+                html = [[NSString alloc] initWithData:data
+                                             encoding:NSUTF16BigEndianStringEncoding];
+
+                if (html == nil) {
+                    html = [[NSString alloc] initWithData:data
+                                                 encoding:NSUTF16LittleEndianStringEncoding];
+                }
+            }
+        }
+    }
+
+    return html;
+}
 
 - (NSObject <HTTPResponse> *)httpResponseForMethod:(NSString *)method URI:(NSString *)path {
 	if (m_package == nil ||
@@ -60,6 +130,25 @@ static RDPackage *m_package = nil;
 
 	NSObject <HTTPResponse> *response = nil;
 
+    NSString * math = @"readium_MathJax.js";
+    if ([path hasPrefix:math]) {
+        NSString *filePath = [[NSBundle mainBundle] pathForResource:@"MathJax" ofType:@"js" inDirectory:@"mathjax"];
+        if (filePath != nil) {
+            NSString *code = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+            if (code != nil) {
+                NSData *data = [code dataUsingEncoding:NSUTF8StringEncoding];
+                if (data != nil) {
+                    RDPackageResourceDataResponse *dataResponse = [[RDPackageResourceDataResponse alloc]
+                            initWithData:data];
+                    dataResponse.contentType = @"text/javascript";
+
+                    response = dataResponse;
+                    return response;
+                }
+            }
+        }
+    }
+
 	// Synchronize using a process-level lock to guard against multiple threads accessing a
 	// resource byte stream, which may lead to instability.
 
@@ -69,29 +158,102 @@ static RDPackage *m_package = nil;
 		if (resource == nil) {
 			NSLog(@"No resource found! (%@)", path);
 		}
-		else if (resource.contentLength < 1000000) {
+		else
+        {
+            bool isHTML = [path hasSuffix:@".html"] || [path hasSuffix:@".xhtml"] || [resource.mimeType isEqualToString:@"application/xhtml+xml"];
 
-			// This resource is small enough that we can just fetch the entire thing in memory,
-			// which simplifies access into the byte stream.  Adjust the threshold to taste.
+            if (isHTML) {
+                NSData *data = resource.data;
+                if (data != nil) {
+                    NSString* source = [self htmlFromData:data];
+                    if (source != nil) {
+                        NSString *pattern = @"(<head.*>)";
+                        NSError *error = nil;
+                        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:&error];
+                        if(error != nil) {
+                            NSLog(@"RegEx error: %@", error);
+                        } else {
+                            //NSString *filePath = [[NSBundle mainBundle] pathForResource:@"epubReadingSystem_inject" ofType:@"js" inDirectory:@"Scripts"];
+                            //NSString *code = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+                            //NSString *inject_epubReadingSystem = [NSString stringWithFormat:@"<script type=\"text/javascript\"></script>", code];
 
-			NSData *data = resource.data;
+                            //NSString *inject_epubReadingSystem1 = [NSString stringWithFormat:@"<script type=\"text/javascript\" src=\"%@/../epubReadingSystem_inject.js\"></script>", m_baseUrlPath];
 
-			if (data != nil) {
-				RDPackageResourceDataResponse *dataResponse = [[RDPackageResourceDataResponse alloc]
-					initWithData:data];
+                            // Installs "hook" function so that top-level window (application) can later inject the window.navigator.epubReadingSystem into this HTML document's iframe
+                            NSString *inject_epubReadingSystem1 = [NSString stringWithFormat:@"<script id=\"readium_epubReadingSystem_inject1\" type=\"text/javascript\">\n//<![CDATA[\n%@\n//]]>\n</script>",
+                                                                                             @"window.readium_set_epubReadingSystem = function (obj) {\
+                                \nwindow.navigator.epubReadingSystem = obj;\
+                                \nwindow.readium_set_epubReadingSystem = undefined;\
+                                \nvar el1 = document.getElementById(\"readium_epubReadingSystem_inject1\");\
+                                \nif (el1 && el1.parentNode) { el1.parentNode.removeChild(el1); }\
+                                \nvar el2 = document.getElementById(\"readium_epubReadingSystem_inject2\");\
+                                \nif (el2 && el2.parentNode) { el2.parentNode.removeChild(el2); }\
+                                \n};"];
 
-				if (resource.mimeType) {
-					dataResponse.contentType = resource.mimeType;
-				}
+                            // Fake script, generates HTTP request => triggers the push of window.navigator.epubReadingSystem into this HTML document's iframe (see LOXWebViewController.mm where the "readium_epubReadingSystem_inject" UIWebView URI query is handled)
+                            NSString *inject_epubReadingSystem2 =
+                                    [NSString stringWithFormat:
+                                    @"<script id=\"readium_epubReadingSystem_inject2\" type=\"text/javascript\" src=\"/%ld/readium_epubReadingSystem_inject.js\"> </script>",
+                                                    m_injectCounter++];
 
-				response = dataResponse;
-			}
-		}
-		else {
-			RDPackageResourceResponse *resourceResponse = [[RDPackageResourceResponse alloc]
-				initWithResource:resource];
-			response = resourceResponse;
-		}
+                            NSString *inject_mathJax = @"";
+                            if ([source rangeOfString:@"<math"].location != NSNotFound) {
+
+                                //inject_mathJax = [NSString stringWithFormat:@"<script type=\"text/javascript\" src=\"%@/../mathjax/MathJax.js\"> </script>", m_baseUrlPath];
+                                inject_mathJax = @"<script type=\"text/javascript\" src=\"/readium_MathJax.js\"> </script>";
+
+                                //NSString *filePath = [[NSBundle mainBundle] pathForResource:@"MathJax" ofType:@"js" inDirectory:@"Scripts/mathjax"];
+                                //NSString *code = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+                                //inject_mathJax = [NSString stringWithFormat:@"<script type=\"text/javascript\">\n//<![CDATA[\n%@\n//]]>\n</script>", code];
+                                //inject_mathJax = [NSString stringWithFormat:@"<script type=\"text/javascript\">\n\n%@\n\n</script>", code];
+                                //inject_mathJax = [NSString stringWithFormat:@"<script type=\"text/javascript\">\n<![CDATA[\n%@\n]]>\n</script>", code];
+                            }
+
+                            NSString *newSource = [regex stringByReplacingMatchesInString:source options:0 range:NSMakeRange(0, [source length]) withTemplate:
+                                    [NSString stringWithFormat:@"%@\n%@\n%@\n%@", @"$1", inject_epubReadingSystem1, inject_epubReadingSystem2, inject_mathJax]];
+                            if (newSource != nil && newSource.length > 0) {
+                                NSData * newData = [newSource dataUsingEncoding:NSUTF8StringEncoding];
+                                if (newData != nil) {
+                                    RDPackageResourceDataResponse *dataResponse = [[RDPackageResourceDataResponse alloc]
+                                            initWithData:newData];
+
+                                    if (resource.mimeType) {
+                                        dataResponse.contentType = resource.mimeType;
+                                    }
+
+                                    response = dataResponse;
+                                    return response;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (resource.contentLength < 1000000) {
+
+                // This resource is small enough that we can just fetch the entire thing in memory,
+                // which simplifies access into the byte stream.  Adjust the threshold to taste.
+
+                NSData *data = resource.data;
+
+                if (data != nil) {
+                    RDPackageResourceDataResponse *dataResponse = [[RDPackageResourceDataResponse alloc]
+                        initWithData:data];
+
+                    if (resource.mimeType) {
+                        dataResponse.contentType = resource.mimeType;
+                    }
+
+                    response = dataResponse;
+                }
+            }
+            else {
+                RDPackageResourceResponse *resourceResponse = [[RDPackageResourceResponse alloc]
+                    initWithResource:resource];
+                response = resourceResponse;
+            }
+        }
 	}
 
 	return response;
@@ -100,6 +262,7 @@ static RDPackage *m_package = nil;
 
 + (void)setPackage:(RDPackage *)package {
 	m_package = package;
+    //m_injectCounter = 0;
 }
 
 
