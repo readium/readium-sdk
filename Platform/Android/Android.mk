9239c1bdbfe36906283e674b3ad6acaa0aa18cf1
Daniel Weck <daniel.weck@gmail.com>
2014-11-22 17:23:17 +0000

    added missing files in Android build (were added by latest iOS / OSX ReadiumSDK updates)

--- Platform/Android/Android.mk
+++ Platform/Android/Android.mk
@@ -1,3 +1,4 @@
+
 LOCAL_PATH := $(call my-dir)/../..
 THIRD_PARTY := $(LOCAL_PATH)/ePub3/ThirdParty
 
@@ -259,6 +260,9 @@ LOCAL_SRC_FILES := \
 		ePub3/ePub/filter_chain.cpp \
 		ePub3/ePub/filter_manager_impl.cpp \
 		ePub3/ePub/filter_manager.cpp \
+		ePub3/ePub/filter_chain_byte_stream_range.cpp \
+		ePub3/ePub/filter_chain_byte_stream.cpp \
+		ePub3/ePub/PassThroughFilter.cpp \
 		ePub3/ePub/font_obfuscation.cpp \
 		ePub3/ePub/glossary.cpp \
 		ePub3/ePub/initialization.cpp \
