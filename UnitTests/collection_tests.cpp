de8b7e91f76af1e2df4e7fd17487083b04cdc3f7
Ric Wright <rkwright@geofx.com>
2014-12-24 10:56:08 -0600

    Updated all the unit tests to have BSD-style license.

--- UnitTests/collection_tests.cpp
+++ UnitTests/collection_tests.cpp
@@ -3,21 +3,15 @@
 //  ePub3
 //
 //  Created by Jim Dovey on 11/29/2013.
-//  Copyright (c) 2012-2013 The Readium Foundation and contributors.
 //
-//  The Readium SDK is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+//  Copyright (c) 2014 Readium Foundation and/or its licensees. All rights reserved.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//  3. Neither the name of the organization nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+
 
 #include "../ePub3/ePub/epub_collection.h"
 #include "../ePub3/ePub/package.h"
