420aee03310602abe5012397cc5b37d439a94f49
DennisPopovDn <den@datanaut.com>
2015-03-12 19:04:12 +0300

    64 bit libzip in progress

--- ePub3/ThirdParty/libzip/zip_unchange.c
+++ ePub3/ThirdParty/libzip/zip_unchange.c
@@ -40,7 +40,7 @@
 
 
 ZIP_EXTERN int
-zip_unchange(struct zip *za, int idx)
+zip_unchange(struct zip *za, zip_uint64_t idx)
 {
     return _zip_unchange(za, idx, 0);
 }
@@ -48,33 +48,25 @@ zip_unchange(struct zip *za, int idx)
 
 
 int
-_zip_unchange(struct zip *za, int idx, int allow_duplicates)
+_zip_unchange(struct zip *za, zip_uint64_t idx, int allow_duplicates)
 {
-    int i;
+    zip_int64_t i;
     
-    if (idx < 0 || idx >= za->nentry) {
+    if (idx >= za->nentry) {
 	_zip_error_set(&za->error, ZIP_ER_INVAL, 0);
 	return -1;
     }
 
-    if (za->entry[idx].ch_filename) {
-	if (!allow_duplicates) {
-	    i = _zip_name_locate(za,
-			 _zip_get_name(za, idx, ZIP_FL_UNCHANGED, NULL),
-				 0, NULL);
-	    if (i != -1 && i != idx) {
-		_zip_error_set(&za->error, ZIP_ER_EXISTS, 0);
-		return -1;
-	    }
+    if (!allow_duplicates && za->entry[idx].changes && (za->entry[idx].changes->changed & ZIP_DIRENT_FILENAME)) {
+	i = _zip_name_locate(za, _zip_get_name(za, idx, ZIP_FL_UNCHANGED, NULL), 0, NULL);
+	if (i >= 0 && (zip_uint64_t)i != idx) {
+	    _zip_error_set(&za->error, ZIP_ER_EXISTS, 0);
+	    return -1;
 	}
-
-	free(za->entry[idx].ch_filename);
-	za->entry[idx].ch_filename = NULL;
     }
 
-    free(za->entry[idx].ch_comment);
-    za->entry[idx].ch_comment = NULL;
-    za->entry[idx].ch_comment_len = -1;
+    _zip_dirent_free(za->entry[idx].changes);
+    za->entry[idx].changes = NULL;
 
     _zip_unchange_data(za->entry+idx);
 
