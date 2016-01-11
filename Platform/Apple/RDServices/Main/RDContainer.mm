8abff8086fbbf19783841fd23980e78e9c0694d9
hslee <hslee@drminside.com>
2016-01-11 16:18:39 +0900

    change try catch

--- Platform/Apple/RDServices/Main/RDContainer.mm
+++ Platform/Apple/RDServices/Main/RDContainer.mm
@@ -65,51 +65,40 @@
 	if (self = [super init]) {
 		m_delegate = delegate;
 
+        
+        ePub3::ErrorHandlerFn sdkErrorHandler = ^(const ePub3::error_details& err) {
+
+            const char * msg = err.message();
+
+            BOOL isSevereEpubError = NO;
+            if (err.is_spec_error()
+                    && (err.severity() == ePub3::ViolationSeverity::Critical
+                    || err.severity() == ePub3::ViolationSeverity::Major))
+                isSevereEpubError = YES;
+
+            BOOL res = [m_delegate container:self handleSdkError:[NSString stringWithUTF8String:msg] isSevereEpubError:isSevereEpubError];
+
+            return (res == YES ? true : false);
+            //return ePub3::DefaultErrorHandler(err);
+        };
+        ePub3::SetErrorHandler(sdkErrorHandler);
+
+        ePub3::InitializeSdk();
+        ePub3::PopulateFilterManager();
+        
+        if ([delegate respondsToSelector:@selector(containerRegisterContentFilters:)]) {
+            [delegate containerRegisterContentFilters:self];
+        }
+        
+        //Content Modules for each DRM library, if any, should be registered in the function.
+        if ([delegate respondsToSelector:@selector(containerRegisterContentModules:)]) {
+               [delegate containerRegisterContentModules:self];
+        }
+
+        m_path = path;
+    
         try {
-            ePub3::ErrorHandlerFn sdkErrorHandler = ^(const ePub3::error_details& err) {
-
-                const char * msg = err.message();
-
-                BOOL isSevereEpubError = NO;
-                if (err.is_spec_error()
-                        && (err.severity() == ePub3::ViolationSeverity::Critical
-                        || err.severity() == ePub3::ViolationSeverity::Major))
-                    isSevereEpubError = YES;
-
-                BOOL res = [m_delegate container:self handleSdkError:[NSString stringWithUTF8String:msg] isSevereEpubError:isSevereEpubError];
-
-                return (res == YES ? true : false);
-                //return ePub3::DefaultErrorHandler(err);
-            };
-            ePub3::SetErrorHandler(sdkErrorHandler);
-
-            ePub3::InitializeSdk();
-            ePub3::PopulateFilterManager();
-            
-            if ([delegate respondsToSelector:@selector(containerRegisterContentFilters:)]) {
-                [delegate containerRegisterContentFilters:self];
-            }
-            
-            //Content Modules for each DRM library, if any, should be registered in the function.
-            if ([delegate respondsToSelector:@selector(containerRegisterContentModules:)]) {
-                   [delegate containerRegisterContentModules:self];
-            }
-
-            m_path = path;
             m_container = ePub3::Container::OpenContainer(path.UTF8String);
-
-            if (m_container == nullptr) {
-                return nil;
-            }
-
-            m_packageList = m_container->Packages();
-            m_packages = [[NSMutableArray alloc] initWithCapacity:4];
-
-            for (auto i = m_packageList.begin(); i != m_packageList.end(); i++) {
-                RDPackage *package = [[RDPackage alloc] initWithPackage:i->get()];
-                [m_packages addObject:package];
-            }
-            
         } catch (std::exception& e) {
             auto msg = e.what();
             [self performSelectorOnMainThread:@selector(epubWarningShow:) withObject:[NSString stringWithUTF8String:msg] waitUntilDone:NO];
@@ -117,6 +106,20 @@
         } catch (...) {
             NSLog(@"unknown exceprion");
         }
+
+        if (m_container == nullptr) {
+            return nil;
+        }
+
+        m_packageList = m_container->Packages();
+        m_packages = [[NSMutableArray alloc] initWithCapacity:4];
+
+        for (auto i = m_packageList.begin(); i != m_packageList.end(); i++) {
+            RDPackage *package = [[RDPackage alloc] initWithPackage:i->get()];
+            [m_packages addObject:package];
+        }
+            
+        
         
 	}
 
