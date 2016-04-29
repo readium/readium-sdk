97882e03453255abf528247c72a038b2df0bcdd3
Olivier Körner <olivierkorner@abm-studio.com>
2016-04-29 12:58:18 +0200

    Adds HTTP caching headers to responses in iOS RDServices

--- Platform/Apple/RDServices/Main/RDPackageResourceDataResponse.m
+++ Platform/Apple/RDServices/Main/RDPackageResourceDataResponse.m
@@ -28,16 +28,61 @@
 //  OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "RDPackageResourceDataResponse.h"
+#import "NSDate+RDDateAsString.h"
+
+NSString * const kDataCacheControlHTTPHeader = @"no-transform,public,max-age=3000,s-maxage=9000";
 
 @implementation RDPackageResourceDataResponse
 
 - (NSDictionary *)httpHeaders {
+    // Add cache-control, expires and last-modified HTTP headers so the webview caches shared assets
+    NSDate *now = [NSDate date];
+    NSString *nowStr = [now dateAsString];
+    NSString *expStr = [[now dateByAddingTimeInterval:60*60*24*10] dateAsString];
 	if (self.contentType) {
-		return @{@"Content-Type": self.contentType};
+		return @{@"Content-Type": self.contentType, @"Cache-Control": kDataCacheControlHTTPHeader, @"Last-Modified": nowStr, @"Expires": expStr};
 	}
 	else {
-		return @{};
+		return @{@"Cache-Control": kDataCacheControlHTTPHeader, @"Last-Modified": nowStr, @"Expires": expStr};
 	}
+}
+ 
+/**
+ * Gets the current date and time, formatted properly (according to RFC) for insertion into an HTTP header.
+ **/
+- (NSString *)dateAsString:(NSDate *)date
+{
+    // From Apple's Documentation (Data Formatting Guide -> Date Formatters -> Cache Formatters for Efficiency):
+    //
+    // "Creating a date formatter is not a cheap operation. If you are likely to use a formatter frequently,
+    // it is typically more efficient to cache a single instance than to create and dispose of multiple instances.
+    // One approach is to use a static variable."
+    //
+    // This was discovered to be true in massive form via issue #46:
+    //
+    // "Was doing some performance benchmarking using instruments and httperf. Using this single optimization
+    // I got a 26% speed improvement - from 1000req/sec to 3800req/sec. Not insignificant.
+    // The culprit? Why, NSDateFormatter, of course!"
+    //
+    // Thus, we are using a static NSDateFormatter here.
+    
+    static NSDateFormatter *df;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        // Example: Sun, 06 Nov 1994 08:49:37 GMT
+        
+        df = [[NSDateFormatter alloc] init];
+        [df setFormatterBehavior:NSDateFormatterBehavior10_4];
+        [df setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"GMT"]];
+        [df setDateFormat:@"EEE, dd MMM y HH:mm:ss 'GMT'"];
+        [df setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
+        
+        // For some reason, using zzz in the format string produces GMT+00:00
+    });
+    
+    return [df stringFromDate:date];
 }
 
 @end
