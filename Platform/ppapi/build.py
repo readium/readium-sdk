f3d22856eb8a274631a1af23f515195eb21e780a
Cyrille Lebeaupin <clebeaupin@noop.fr>
2016-07-05 16:41:15 +0200

    Fix build system on win32

--- Platform/ppapi/build.py
+++ Platform/ppapi/build.py
@@ -4,24 +4,29 @@ import subprocess
 import platform
 import utils
 
-system = platform.system().lower()
+SYSTEM = platform.system().lower()
 
-if system == "linux":
+if SYSTEM == "linux":
     # Use clang on linux
     # print "Use clang compiler"
     # os.environ["CC"] = "clang"
     # os.environ["CXX"] = "clang++"
     # os.environ["GYP_DEFINES"] = "clang=1"
     pass
-elif system == "windows":
+elif SYSTEM == "windows":
     # Use msvs version 2015
     print "Use msvs version 2015"
     os.environ["GYP_MSVS_VERSION"] = "2015"
 
 # Directories variables
-PPAPI_PATH = os.getcwd()
+PPAPI_PATH = os.path.abspath(os.getcwd())
 GYP_CMD = os.path.join(PPAPI_PATH, "vendor", "gyp", "gyp_main.py")
-NINJA_CMD = os.path.join(PPAPI_PATH, "vendor", "ninja", "ninja")
+NINJA_PATH = os.path.join(PPAPI_PATH, "vendor", "ninja")
+
+if SYSTEM == "windows":
+    NINJA_CMD = os.path.join(NINJA_PATH, "ninja.exe")
+else:
+    NINJA_CMD = os.path.join(NINJA_PATH, "ninja")
 
 # Create ninja build file
 print "Create ninja project"
@@ -29,4 +34,9 @@ utils.execute_command(["python", GYP_CMD, "--depth=.", "-f", "ninja", "ppapi.gyp
 
 # Build ninja project
 print "Build ninja project"
-utils.execute_command([NINJA_CMD, "-C", os.path.join("out", "Default"), "-f", "build.ninja"])
+cmd = [NINJA_CMD, "-C", os.path.join("out", "Default"), "-f", "build.ninja"]
+
+if SYSTEM == "windows":
+    cmd = ["vcvarsall.bat", "&&"] + cmd
+
+utils.execute_command(cmd)
