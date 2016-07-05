8abe95d5e4dd47481e2f07deb2353d9ea0258c23
Cyrille Lebeaupin <clebeaupin@noop.fr>
2016-07-05 16:39:01 +0200

    Convert working directory in absolute path

--- Platform/ppapi/utils.py
+++ Platform/ppapi/utils.py
@@ -1,6 +1,7 @@
 import subprocess
+import os.path
 
 # Helper to execute command
 def execute_command(cmd, cwd=None):
-    p = subprocess.Popen(cmd, cwd=cwd)
+    p = subprocess.Popen(cmd, cwd=os.path.abspath(cwd))
     p.wait()
