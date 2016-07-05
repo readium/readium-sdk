495b77b4811025e0513dd5a02ba79f2a38a97c95
Cyrille Lebeaupin <clebeaupin@noop.fr>
2016-07-05 10:08:49 +0200

    Migrate bootstrap bash script to python script

--- Platform/ppapi/bootstrap.py
+++ Platform/ppapi/bootstrap.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 import os
 import shutil
+import subprocess
+import utils
+import urllib
+import zipfile
+import tarfile
+import tempfile
 
 # Directories variables
 PPAPI_PATH = os.getcwd()
@@ -39,7 +45,6 @@ INCLUDE_MAPPING = (
     ))
 )
 
-
 def copy_header_files(src, dst):
     if not os.path.exists(dst):
         os.mkdir(dst)
@@ -53,7 +58,7 @@ def copy_header_files(src, dst):
         )
 
 # Copy includes in a specific directory
-def build_includes():
+def install_includes():
     # Create include dirname
     if not os.path.exists(INCLUDE_PATH):
         os.mkdir(INCLUDE_PATH)
@@ -64,6 +69,85 @@ def build_includes():
         for srcPath in srcPaths:
             copy_header_files(srcPath, dstPath)
 
+# Install gyp
+def install_gyp():
+    if os.path.exists(os.path.join("vendor", "gyp")):
+        return
+    
+    print "Clone gyp"
+    utils.execute_command(["git", "clone", "https://chromium.googlesource.com/external/gyp.git", "vendor/gyp"])
+
+# Install ninja
+def install_ninja():
+    if not os.path.exists(os.path.join("vendor", "ninja")):
+        print "Clone ninja"
+        utils.execute_command(["git", "clone", "https://github.com/ninja-build/ninja.git", "vendor/ninja"])
+
+    # Configure and build ninja
+    if not os.path.exists(os.path.join("vendor", "ninja", "ninja")):
+        print "Build and install ninja"
+        utils.execute_command(["python", "configure.py", "--bootstrap"], os.path.join("vendor", "ninja"))
+
+# Install nacl sdk
+def install_nacl_sdk():
+    nacl_sdk_path = os.path.join("vendor", "nacl_sdk")
+
+    if not os.path.exists(nacl_sdk_path):
+        print "Download nacl sdk"
+        nacl_sdk_zip_path = os.path.join("vendor", "nacl_sdk.zip")
+        urllib.urlretrieve(
+            "https://storage.googleapis.com/nativeclient-mirror/nacl/nacl_sdk/nacl_sdk.zip", 
+            nacl_sdk_zip_path)
+        
+        print "Extract nacl sdk"
+        with zipfile.ZipFile(nacl_sdk_zip_path, "r") as nacl_sdk_zip:
+            tmp_nacl_sdk_path = tempfile.mkdtemp(prefix="nacl", dir="vendor")
+    	    nacl_sdk_zip.extractall(tmp_nacl_sdk_path)
+            shutil.move(
+                os.path.join(tmp_nacl_sdk_path, "nacl_sdk"),
+                nacl_sdk_path)
+            os.rmdir(tmp_nacl_sdk_path)
+
+        os.remove(nacl_sdk_zip_path)
+
+    if not os.path.exists(os.path.join(nacl_sdk_path, "pepper_49")):
+        print "Install nacl_sdk"
+        os.chmod(os.path.join(nacl_sdk_path, "naclsdk"), 0755) 
+        utils.execute_command(["./naclsdk", "install", "pepper_49"], nacl_sdk_path)
+
+
+# Install libxml2
+def install_libxml2():
+    libxml2_path = os.path.join("vendor", "libxml2")
+
+    if os.path.exists(libxml2_path):
+        return
+    
+    print "Download libxml2"
+    libxml2_tar_path = os.path.join("vendor", "libxml2.tar.gz")
+    urllib.urlretrieve(
+            "http://xmlsoft.org/sources/libxml2-2.9.4.tar.gz", 
+            libxml2_tar_path)
+
+    print "Extract libxml2"
+    with tarfile.open(libxml2_tar_path, "r:gz") as libxml2_tar:
+        tmp_libxml2_path = tempfile.mkdtemp(prefix="libxml2", dir="vendor")
+        libxml2_tar.extractall(tmp_libxml2_path)
+        shutil.move(
+            os.path.join(tmp_libxml2_path, "libxml2-2.9.4"),
+            libxml2_path)
+        os.rmdir(tmp_libxml2_path)
+
+    os.remove(libxml2_tar_path)
+
+# Download and install vendors
+if not os.path.exists("vendor"):
+    os.mkdir("vendor")
+
+install_gyp()
+install_ninja()
+install_nacl_sdk()
+install_libxml2()
 
 # Build includes
-build_includes()
+install_includes()
