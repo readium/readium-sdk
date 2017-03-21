f0042ab6fab62864962f344e39a9dba78b3359c0
danielweck <daniel.weck@gmail.com>
2017-03-21 18:09:20 +0000

    commented-out unused backup-atomics (Android GCC vs. CLANG, NDK now updated, but v14 still not final recommendation for Clang (GCC deprecation))

--- Platform/Android/epub3/src/main/jni/android/backup_atomics.cpp
+++ Platform/Android/epub3/src/main/jni/android/backup_atomics.cpp
@@ -17,87 +17,87 @@
 //  Affero General Public License as published by the Free Software Foundation, either version 3 of 
 //  the License, or (at your option) any later version. You should have received a copy of the GNU 
 //  Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#if 1
-#include <pthread.h>
-int ___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__ = 0;
-extern "C" void __lock_atomics(void)
-{
-    while (1)
-    {
-        int i;
-        for (i=0; i<10000;i++)
-        {
-            if (__sync_bool_compare_and_swap(&___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__, 0, 1))
-            {
-                return;
-            }
-        }
-        sched_yield();
-    }
-}
-extern "C" void __unlock_atomics(void)
-{
-    __asm__ __volatile__ ("" ::: "memory");
-    *(&___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__) = 0;
-}
-
-extern "C" void* __atomic_load(void* const volatile* obj, int __m)
-{
-    return *obj;
-}
-#else
-#include <mutex>
-
-std::mutex ___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__;
-
-extern "C" void*
-__atomic_load(void* const volatile* obj)
-{
-    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
-    return *obj;
-}
-
-extern "C" void
-__atomic_store(void* volatile* obj, void* desr)
-{
-    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
-    *obj = desr;
-}
-
-extern "C" void*
-__atomic_exchange(void* volatile* obj, void* desr)
-{
-    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
-    void* r = *obj;
-    *obj = desr;
-    return r;
-}
-
-extern "C" void*
-__atomic_fetch_add(void* volatile* obj, ptrdiff_t operand)
-{
-    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
-    void* r = *obj;
-    (char*&)(*obj) += operand;
-    return r;
-}
-
-extern "C" void*
-__atomic_fetch_sub(void* volatile* obj, ptrdiff_t operand)
-{
-    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
-    void* r = *obj;
-    (char*&)(*obj) -= operand;
-    return r;
-}
-
-extern "C" void __atomic_thread_fence()
-{
-    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
-}
-
-extern "C" void __atomic_signal_fence()
-{
-    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
-}
-#endif
+//#if 1
+//#include <pthread.h>
+//int ___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__ = 0;
+//extern "C" void __lock_atomics(void)
+//{
+//    while (1)
+//    {
+//        int i;
+//        for (i=0; i<10000;i++)
+//        {
+//            if (__sync_bool_compare_and_swap(&___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__, 0, 1))
+//            {
+//                return;
+//            }
+//        }
+//        sched_yield();
+//    }
+//}
+//extern "C" void __unlock_atomics(void)
+//{
+//    __asm__ __volatile__ ("" ::: "memory");
+//    *(&___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__) = 0;
+//}
+//
+//extern "C" void* __atomic_load(void* const volatile* obj, int __m)
+//{
+//    return *obj;
+//}
+//#else
+//#include <mutex>
+//
+//std::mutex ___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__;
+//
+//extern "C" void*
+//__atomic_load(void* const volatile* obj)
+//{
+//    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
+//    return *obj;
+//}
+//
+//extern "C" void
+//__atomic_store(void* volatile* obj, void* desr)
+//{
+//    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
+//    *obj = desr;
+//}
+//
+//extern "C" void*
+//__atomic_exchange(void* volatile* obj, void* desr)
+//{
+//    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
+//    void* r = *obj;
+//    *obj = desr;
+//    return r;
+//}
+//
+//extern "C" void*
+//__atomic_fetch_add(void* volatile* obj, ptrdiff_t operand)
+//{
+//    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
+//    void* r = *obj;
+//    (char*&)(*obj) += operand;
+//    return r;
+//}
+//
+//extern "C" void*
+//__atomic_fetch_sub(void* volatile* obj, ptrdiff_t operand)
+//{
+//    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
+//    void* r = *obj;
+//    (char*&)(*obj) -= operand;
+//    return r;
+//}
+//
+//extern "C" void __atomic_thread_fence()
+//{
+//    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
+//}
+//
+//extern "C" void __atomic_signal_fence()
+//{
+//    std::unique_lock<std::mutex> _(___OMFG_WHERE_ARE_ALL_THE_ATOMIC_INTRINSICS__);
+//}
+//#endif
--- Platform/Android/epub3/src/main/jni/android/gnucxx-clang/bits/atomic_lockfree_defines.h
+++ Platform/Android/epub3/src/main/jni/android/gnucxx-clang/bits/atomic_lockfree_defines.h
@@ -27,33 +27,33 @@
 
 #ifndef _GLIBCXX_ATOMIC_LOCK_FREE_H
 #define _GLIBCXX_ATOMIC_LOCK_FREE_H 1
-
-#pragma GCC system_header
-
-/**
- * @addtogroup atomics
- * @{
- */
-
-/**
- * Lock-free property.
- *
- * 0 indicates that the types are never lock-free.
- * 1 indicates that the types are sometimes lock-free.
- * 2 indicates that the types are always lock-free.
- */
-
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-#define ATOMIC_CHAR_LOCK_FREE 0
-#define ATOMIC_CHAR16_T_LOCK_FREE 0
-#define ATOMIC_CHAR32_T_LOCK_FREE 0
-#define ATOMIC_WCHAR_T_LOCK_FREE 0
-#define ATOMIC_SHORT_LOCK_FREE 0
-#define ATOMIC_INT_LOCK_FREE 0
-#define ATOMIC_LONG_LOCK_FREE 0
-#define ATOMIC_LLONG_LOCK_FREE 0
-#endif
-
-// @} group atomics
+//
+//#pragma GCC system_header
+//
+///**
+// * @addtogroup atomics
+// * @{
+// */
+//
+///**
+// * Lock-free property.
+// *
+// * 0 indicates that the types are never lock-free.
+// * 1 indicates that the types are sometimes lock-free.
+// * 2 indicates that the types are always lock-free.
+// */
+//
+//#ifdef __GXX_EXPERIMENTAL_CXX0X__
+//#define ATOMIC_CHAR_LOCK_FREE 0
+//#define ATOMIC_CHAR16_T_LOCK_FREE 0
+//#define ATOMIC_CHAR32_T_LOCK_FREE 0
+//#define ATOMIC_WCHAR_T_LOCK_FREE 0
+//#define ATOMIC_SHORT_LOCK_FREE 0
+//#define ATOMIC_INT_LOCK_FREE 0
+//#define ATOMIC_LONG_LOCK_FREE 0
+//#define ATOMIC_LLONG_LOCK_FREE 0
+//#endif
+//
+//// @} group atomics
 
 #endif
